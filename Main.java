93505bb7a0f551c07f8217af37dcb0559f35856d
TiTTO-Pro <91017135+TiTTO-Pro@users.noreply.github.com>
2022-02-21 18:56:19 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter (k) -> ");
+        int k = input.nextInt();
+        new FrameSet(k);
+
+    }
+}
+
+
+
+
+
+
+
+
