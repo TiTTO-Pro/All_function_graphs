b840752eba5cd89bec401ab37ad1caaecf2b47fc
TiTTO-Pro <91017135+TiTTO-Pro@users.noreply.github.com>
2022-02-21 19:05:22 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        
+        /*Scanner input = new Scanner(System.in);}
+        System.out.print("Enter (k) -> ");       } - for graph 'y = k / x'
+        int k = input.nextInt();                 }
+         */
 
-        System.out.print("Enter (k) -> ");
-        int k = input.nextInt();
-        new FrameSet(k);
+        
+        new FrameSet(); // enter 'k' in (...) if u want launch 'y = k / x' graph, and add 'int k' in constructor FrameSet(...)
 
     }
 }
